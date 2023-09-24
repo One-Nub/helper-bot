81b14d336884af3ca9ad25f98bce6c150ee8f13e
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-24 21:22:34 +0000

    Add button handler register function

---
+++ src/modules/events/on_interaction.py
@@ -0,0 +1,28 @@
+import logging
+
+from discord import Interaction, InteractionType
+
+from helper_bot import instance as bot
+
+
+@bot.event
+async def on_interaction(interaction: Interaction):
+    # Handle button interactions with custom handler
+    match interaction.type:
+        case InteractionType.component:
+            for name, handler in bot.button_handlers.items():
+                custom_id: str = interaction.data.get("custom_id", "")
+
+                if not custom_id.startswith(name):
+                    continue
+
+                await handler(interaction)
+
+        case InteractionType.application_command:
+            pass
+
+        case InteractionType.autocomplete:
+            pass
+
+        case InteractionType.modal_submit:
+            pass
