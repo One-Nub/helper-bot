63965dfc15a447c0c11f0b95517481197b569fb8
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-24 19:19:09 +0000

    Add basic tag command layout

--- src/modules/commands/tag.py
+++ src/modules/commands/tag.py
@@ -1,6 +1,18 @@
+from discord.ext.commands import Context
+
 from helper_bot import instance as bot
 
 
-@bot.hybrid_command("tag")
-async def tag(ctx):
-    print("hello world")
+@bot.hybrid_group("tag", description="Send a tag to this channel!", fallback="send")
+async def tag(ctx: Context):
+    await ctx.reply("hello world")
+
+
+@tag.command("add", description="Add a tag to the tag list.")
+async def add_tag(ctx: Context):
+    await ctx.reply("attempted to add a tag")
+
+
+@tag.command("delete", description="Remove a tag from the tag list.")
+async def delete_tag(ctx: Context):
+    await ctx.reply("attempted to remove a tag")
