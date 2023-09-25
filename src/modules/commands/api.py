3ddce4fdda7493cbf23e28dd2f97cba2e936e416
lxphere <47260747+lxphere@users.noreply.github.com>
2023-09-25 13:33:44 -0400

    finish api command (guild-only)

--- src/modules/commands/api.py
+++ src/modules/commands/api.py
@@ -2,7 +2,10 @@ from discord.ext.commands import Context
 from helper_bot import instance as bot
 import requests
 import discord
+import json
 import config
+from datetime import datetime
+from constants import RED, BLURPLE
 
 ## api command
 
@@ -17,12 +20,22 @@ async def api(ctx: Context, lookup_id: int = 0):
             raise Exception("Invalid Argument `id`. Please provide a valid Discord ID.")
         else:
             guild = 372036754078826496
-            print(guild)
-            string = 'https://api.blox.link/v4/public/guilds/{}/discord-to-roblox/{}'.format(guild, lookup_id)
-            x = requests.get(string, headers={"Authorization": config.API_KEY})
-            print(x.json())
+            string = f'https://api.blox.link/v4/public/guilds/{guild}/discord-to-roblox/{lookup_id}'
+            x = requests.get(string, headers={"Authorization" : config.API_KEY})
+            response = x.json()
+            if(response.get("resolved") != None):
+                del response["resolved"]
+            success_embed = discord.Embed()
+            success_embed.title = "Success"
+            success_embed.description= f"Sent a request to {string}.\n\n**Response**\n```json\n{response}```"
+            success_embed.color = BLURPLE
+            success_embed.timestamp = datetime.now()
+            success_embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
+            await ctx.reply(embed=success_embed, mention_author=False)
     except Exception as Error: 
-        embed_var = discord.Embed(title="Error", description=Error, color=0x00ff00)
+        embed_var = discord.Embed(title="<:BloxlinkDead:823633973967716363> Error", description=Error, color=RED)
+        embed_var.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
+        embed_var.timestamp = datetime.now()
         await ctx.reply(embed=embed_var)
   
    # x = requests.GET("https://api.blox.link/v1/user/" + lookup_id)
