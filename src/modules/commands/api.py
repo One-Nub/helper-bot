8a074f57780a794948024f1294df974627684a10
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-26 01:12:05 +0000

    Format files, improve readability

--- src/modules/commands/api.py
+++ src/modules/commands/api.py
@@ -1,43 +1,54 @@
-from discord.ext.commands import Context
-from helper_bot import instance as bot
-import requests
-import discord
 import json
-import config
 from datetime import datetime
-from constants import RED, BLURPLE
 
-## api command
+import discord
+import requests
+from discord.ext.commands import Context
+
+import config
+from constants import BLURPLE, RED
+from helper_bot import instance as bot
+
 
 @bot.command("api", description="Fetch information via Bloxlink API.")
 async def api(ctx: Context, lookup_id: int = 0):
-    """Fetch information via Bloxlink API!"""    
-    ## get the bot latency and round it, multiply by 1000
+    """Fetch information from the Bloxlink API!"""
     try:
-        if(lookup_id == 0):
+        if lookup_id == 0:
             raise Exception("Missing argument `id`. Please provide a valid Discord ID.")
-        elif(len(str(lookup_id)) < 17):
+
+        elif len(str(lookup_id)) < 17:
             raise Exception("Invalid Argument `id`. Please provide a valid Discord ID.")
+
         else:
             guild = 372036754078826496
-            string = f'https://api.blox.link/v4/public/guilds/{guild}/discord-to-roblox/{lookup_id}'
-            x = requests.get(string, headers={"Authorization" : config.API_KEY})
+            string = f"https://api.blox.link/v4/public/guilds/{guild}/discord-to-roblox/{lookup_id}"
+
+            x = requests.get(string, headers={"Authorization": config.API_KEY})
             response = x.json()
-            if(response.get("resolved") != None):
+
+            if response.get("resolved") != None:
                 del response["resolved"]
+
             success_embed = discord.Embed()
             success_embed.title = "Success"
-            success_embed.description= f"Sent a request to {string}.\n\n**Response**\n```json\n{response}```"
+            success_embed.description = f"Sent a request to {string}.\n\n**Response**\n```json\n{response}```"
             success_embed.color = BLURPLE
             success_embed.timestamp = datetime.now()
             success_embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
+
             await ctx.reply(embed=success_embed, mention_author=False)
-    except Exception as Error: 
-        embed_var = discord.Embed(title="<:BloxlinkDead:823633973967716363> Error", description=Error, color=RED)
+    except Exception as Error:
+        embed_var = discord.Embed(
+            title="<:BloxlinkDead:823633973967716363> Error",
+            description=Error,
+            color=RED,
+        )
         embed_var.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
         embed_var.timestamp = datetime.now()
+
         await ctx.reply(embed=embed_var)
-  
-   # x = requests.GET("https://api.blox.link/v1/user/" + lookup_id)
-    # await ctx.reply("Pong! - " + str(latency) + "ms")
-    
+
+
+# x = requests.GET("https://api.blox.link/v1/user/" + lookup_id)
+# await ctx.reply("Pong! - " + str(latency) + "ms")
--- src/modules/commands/ping.py
+++ src/modules/commands/ping.py
@@ -1,13 +1,11 @@
 from discord.ext.commands import Context
+
 from helper_bot import instance as bot
 
 
-## ping command
-
 @bot.command("ping", description="Check the bot's latency.")
 async def ping(ctx: Context):
-    """find out what the bot latency is!"""    
+    """Find out what the bot latency is!"""
     ## get the bot latency and round it, multiply by 1000
     latency = round((ctx.bot.latency) * 1000)
     await ctx.reply("Pong! - " + str(latency) + "ms")
-    
