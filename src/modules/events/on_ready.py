ac35225b82970e2278de083a737319ea140e6804
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-26 04:44:39 +0000

    Set bot status on ready

--- src/modules/events/on_ready.py
+++ src/modules/events/on_ready.py
@@ -1,10 +1,16 @@
 import logging
 
+from discord import CustomActivity, Status
+
 from resources.helper_bot import instance as bot
 
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(
+        activity=CustomActivity(name="Helping in the support channel!"),
+        status=Status.online,
+    )
     logging.info("Syncing slash commands...")
     await bot.tree.sync()
     logging.info("Bot has been initialized.")
