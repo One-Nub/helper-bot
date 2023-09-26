55b839689aba9ab021b00d86131ab7ea64b5aad4
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-26 02:30:36 +0000

    Check for whitelisted uses in is_staff

--- src/resources/checks.py
+++ src/resources/checks.py
@@ -1,13 +1,16 @@
+from discord import Interaction
 from discord.ext.commands import Context
 
 from resources.constants import ADMIN_ROLES, WHITELISTED_USERS
 
 
-async def is_staff(ctx: Context) -> bool:
-    if ctx.author.id in WHITELISTED_USERS:
+async def is_staff(ctx: (Context | Interaction)) -> bool:
+    author = ctx.author if isinstance(ctx, Context) else ctx.user
+
+    if author.id in WHITELISTED_USERS:
         return True
 
-    roles = set([role.id for role in ctx.author.roles])
+    roles = set([role.id for role in author.roles])
     admin_roles = set(ADMIN_ROLES.values())
 
     return len(roles.intersection(admin_roles)) != 0
