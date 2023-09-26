78408d2cde6599b6645571b73f7a0e1ca9e3fdfd
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-26 18:19:48 +0000

    Update status, include bot info in start message

--- src/modules/events/on_ready.py
+++ src/modules/events/on_ready.py
@@ -8,9 +8,11 @@ from resources.helper_bot import instance as bot
 @bot.event
 async def on_ready():
     await bot.change_presence(
-        activity=CustomActivity(name="Helping in the support channel!"),
+        activity=CustomActivity(name="Your local helper bot!"),
         status=Status.online,
     )
+
     logging.info("Syncing slash commands...")
     await bot.tree.sync()
-    logging.info("Bot has been initialized.")
+
+    logging.info(f"Bot ({bot.user.name}#{bot.user.discriminator}) has finished initializing!")
