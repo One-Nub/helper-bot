22f3a76fae4c711c9368b8e16dc3d531e711c1a2
lxphere <47260747+lxphere@users.noreply.github.com>
2023-09-28 16:45:22 -0400

    cleanup

--- src/modules/commands/reset_tag.py
+++ src/modules/commands/reset_tag.py
@@ -7,22 +7,21 @@ from discord.ext.commands import Context, check
 from resources.constants import BLURPLE, RED
 from resources.checks import is_dev
 from resources.helper_bot import instance as bot
-from resources.secrets import BLOXLINK_API_KEY  # pylint: disable=E0611
 
 
 @bot.command("reset_tag", description="Reset some information about a tag.", aliases=["rt"])
 @check(is_dev)
-async def reset_tag(ctx: Context, tag: str = "0"):
+async def reset_tag(ctx: Context, tag_name: str = "0"):
     """Reset tag use count."""
     try:
-        if tag == "0":
-            raise Exception("Missing argument `tag`. Please provide a valid tag name.")
+        if tag_name == "0":
+            raise Exception("Missing argument `tag_name`. Please provide a valid tag name.")
         
         else:
-             tag = await bot.db.get_tag(name)
-             if tag is None:
-                 raise Exception(f'The tag "{name}" was not found!')
-             await bot.db.update_tag(
+            tag = await bot.db.get_tag(tag_name)
+            if tag is None:
+                raise Exception(f'The tag "{tag_name}" was not found!')
+            await bot.db.update_tag(
                 tag['_id'],
                 tag['content'],
                 use_count=0,
@@ -38,7 +37,3 @@ async def reset_tag(ctx: Context, tag: str = "0"):
         embed_var.timestamp = datetime.now()
 
         await ctx.reply(embed=embed_var)
-
-
-# x = requests.GET("https://api.blox.link/v1/user/" + lookup_id)
-# await ctx.reply("Pong! - " + str(latency) + "ms")
