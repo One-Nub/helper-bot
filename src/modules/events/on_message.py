e5f4e697fb6a5b5411a8fdabd65e192d4b4d536e
lxphere <47260747+lxphere@users.noreply.github.com>
2023-09-29 21:08:05 -0400

    staff check

--- src/modules/events/on_message.py
+++ src/modules/events/on_message.py
@@ -1,8 +1,14 @@
 from resources.helper_bot import instance as bot
 from datetime import datetime
+from resources.constants import ADMIN_ROLES
 @bot.event
 async def on_message(message):
     if not message.author.bot and message.channel.id == 372181186816245770:
+        roles = set([role.id for role in message.author.roles])
+        admin_roles = set(ADMIN_ROLES.values())
+    if len(roles.intersection(admin_roles)) == 0:
+        return
+    else:
         try:
             data = await bot.db.get_staff_metrics(message.author.id)
             if data is None:
