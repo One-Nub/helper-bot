56e77fec168687e4b20557770aac6ee09ef73392
lxphere <47260747+lxphere@users.noreply.github.com>
2023-09-30 17:18:17 -0400

    refresh to activity stuff yay

--- src/modules/commands/activity.py
+++ src/modules/commands/activity.py
@@ -1,16 +1,56 @@
-from datetime import datetime
+import math
+from datetime import datetime, timedelta
 
 import discord
+from discord import ui
 from discord.ext.commands import Context, check
 
 from resources.checks import is_cm
-from resources.constants import BLURPLE, RED
+from resources.constants import BLURPLE, RED, UNICODE_LEFT, UNICODE_RIGHT
 from resources.helper_bot import instance as bot
 
+MAX_USERS_PER_PAGE = 10
 
-@bot.command("activity", description="Staff activity check.")
+
+@bot.hybrid_group("activity", description="Send (staff) activity leaderboard to this channel!", fallback="lb")
 @check(is_cm)
-async def activity(ctx: Context, staff_id: int = 0):
+async def activity_base(ctx: Context):
+    """Activity of all Staff members."""
+    # Get activity from the db, get their msg_count, then sort by highest to lowest
+    activity_list = await bot.db.get_all_staff_metrics()
+    activity_list.sort(key=lambda x: x["msg_count"], reverse=True)
+    description = ""
+    for i, tag in enumerate(activity_list):
+        description += f"{i+1}. <@{tag['_id']}> - {tag['msg_count']} message(s)\n"
+    embed = discord.Embed(
+        title="Activity Leaderboard",
+        description=description,
+        color=BLURPLE,
+    )
+    await ctx.reply(embed=embed)
+
+
+@activity_base.command("trials", description="View activity for trials.")
+@check(is_cm)
+async def trials(ctx: Context):
+    """Activity of all Trial members."""
+    # Get activity from the db, get their msg_count, then sort by highest to lowest
+    activity_list = await bot.db.get_all_trial_metrics()
+    activity_list.sort(key=lambda x: x["msg_count"], reverse=True)
+    description = ""
+    for i, tag in enumerate(activity_list):
+        description += f"{i+1}. <@{tag['_id']}> - {tag['msg_count']} message(s)\n"
+    embed = discord.Embed(
+        title="Activity Leaderboard [Trial Staff]",
+        description=description,
+        color=BLURPLE,
+    )
+    await ctx.reply(embed=embed, mention_author=False, ephemeral=True)
+
+
+@activity_base.command("view", description="View activity for specific user.")
+@check(is_cm)
+async def activity_view(ctx: Context, staff_id: int = 0):
     """Fetch information about activity from a certain user."""
     try:
         if staff_id == 0:
@@ -32,7 +72,7 @@ async def activity(ctx: Context, staff_id: int = 0):
             success_embed.timestamp = datetime.now()
             success_embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
 
-            await ctx.reply(embed=success_embed, mention_author=False)
+            await ctx.reply(embed=success_embed, mention_author=False, ephemeral=True)
     except Exception as Error:
         embed_var = discord.Embed(
             title="<:BloxlinkDead:823633973967716363> Error",
--- src/modules/events/on_message.py
+++ src/modules/events/on_message.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from resources.constants import ADMIN_ROLES
+from resources.constants import ADMIN_ROLES, TRIAL_ROLE
 from resources.helper_bot import instance as bot
 
 
@@ -9,19 +9,21 @@ async def on_message(message):
     if not message.author.bot and message.channel.id == 372181186816245770:
         roles = set([role.id for role in message.author.roles])
         admin_roles = set(ADMIN_ROLES.values())
-        if len(roles.intersection(admin_roles)) != 0:
+        if len(roles.intersection(admin_roles)) != 0 or TRIAL_ROLE in roles:
             try:
                 data = await bot.db.get_staff_metrics(message.author.id)
                 if data is None:
                     await bot.db.update_staff_metric(
                         message.author.id,
                         1,
+                        "Trial" if TRIAL_ROLE in roles else "Staff",
                         datetime.now(),
                     )
                 else:
                     await bot.db.update_staff_metric(
                         message.author.id,
                         data["msg_count"] + 1,
+                        "Trial" if TRIAL_ROLE in roles else "Staff",
                         datetime.now(),
                     )
             except Exception as e:
