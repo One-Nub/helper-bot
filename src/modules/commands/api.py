e12ae55d5d56203db36668912007b17f41994178
Nub <38899321+One-Nub@users.noreply.github.com>
2023-11-09 22:14:03 +0000

    Add context menu API query trigger

--- src/modules/commands/api.py
+++ src/modules/commands/api.py
@@ -9,14 +9,16 @@ from resources.constants import BLURPLE, RED
 from resources.helper_bot import instance as bot
 from resources.secrets import BLOXLINK_API_KEY  # pylint: disable=E0611
 
+BLOXLINK_GUILD = 372036754078826496
+
 
 @bot.command("api", description="Fetch information via Bloxlink API.")
 @check(is_staff_or_trial)
 async def api(ctx: Context, lookup_id: int = 0):
     """Fetch information from the Bloxlink API!"""
-    guild = 372036754078826496
+
     try:
-        if ctx.guild.id != guild:
+        if ctx.guild.id != BLOXLINK_GUILD:
             raise Exception("This command can only be used in the Bloxlink HQ Server!")
 
         elif lookup_id == 0:
@@ -25,31 +27,10 @@ async def api(ctx: Context, lookup_id: int = 0):
         elif len(str(lookup_id)) < 17:
             raise Exception("Invalid Argument `id`. Please provide a valid Discord ID.")
 
-        else:
-            string = f"https://api.blox.link/v4/public/discord-to-roblox/{lookup_id}"
+        response_embed, response_buttons = await api_request_handler(lookup_id)
+        response_embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
 
-            x = requests.get(string, headers={"Authorization": BLOXLINK_API_KEY}, timeout=5)
-            response = x.json()
-
-            if response.get("resolved") != None:
-                del response["resolved"]
-
-            success_embed = discord.Embed()
-            success_embed.title = "Success"
-            success_embed.description = f"Sent a request to {string}.\n\n**Response**\n```json\n{response}```"
-            success_embed.color = BLURPLE
-            success_embed.timestamp = datetime.now()
-            success_embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
-
-            view = discord.ui.View()
-            view.add_item(
-                discord.ui.Button(
-                    label="Roblox Profile",
-                    url=f"https://roblox.com/users/{response.get('robloxID', 0)}/profile",
-                )
-            )
-
-            await ctx.reply(embed=success_embed, mention_author=False, view=view)
+        await ctx.reply(embed=response_embed, mention_author=False, view=response_buttons)
     except Exception as Error:
         embed_var = discord.Embed(
             title="<:BloxlinkDead:823633973967716363> Error",
@@ -62,5 +43,68 @@ async def api(ctx: Context, lookup_id: int = 0):
         await ctx.reply(embed=embed_var)
 
 
-# x = requests.GET("https://api.blox.link/v1/user/" + lookup_id)
-# await ctx.reply("Pong! - " + str(latency) + "ms")
+@discord.app_commands.context_menu(name="Bloxlink API Lookup")
+async def api_menu(interaction: discord.Interaction, user: discord.Member):
+    if interaction.guild_id != BLOXLINK_GUILD:
+        await interaction.response.send_message(
+            content="This context menu only works in Bloxlink HQ!",
+            ephemeral=True,
+            allowed_mentions=discord.AllowedMentions(users=False),
+        )
+        return
+
+    allowed_to_run = await is_staff_or_trial(interaction)
+    if not allowed_to_run:
+        await interaction.response.send_message(
+            content="You are not allowed to use this context menu!",
+            ephemeral=True,
+            allowed_mentions=discord.AllowedMentions(users=False),
+        )
+        return
+
+    response_embed, response_buttons = await api_request_handler(user.id)
+    response_embed.set_footer(text="Bloxlink Helper", icon_url=interaction.user.display_avatar)
+
+    await interaction.response.send_message(
+        embed=response_embed,
+        view=response_buttons,
+        ephemeral=True,
+        allowed_mentions=discord.AllowedMentions(users=False),
+    )
+
+
+async def api_request_handler(user_id: int) -> tuple:
+    string = f"https://api.blox.link/v4/public/discord-to-roblox/{user_id}"
+
+    embed = discord.Embed()
+    embed.timestamp = datetime.now()
+    embed.color = BLURPLE
+
+    try:
+        req = requests.get(string, headers={"Authorization": BLOXLINK_API_KEY}, timeout=5)
+        response = req.json()
+    except (requests.ConnectionError, requests.HTTPError, requests.Timeout) as exc:
+        embed.title = "<:BloxlinkDead:823633973967716363> Error"
+        embed.description = f"An issue was encountered querying the Bloxlink api! - ({exc})"
+        embed.color = RED
+
+        return (embed, None)
+
+    if response.get("resolved") is not None:
+        del response["resolved"]
+
+    embed.title = "Success"
+    embed.description = f"Sent a request to {string}.\n\n**Response**\n```json\n{response}```"
+
+    view = discord.ui.View()
+    view.add_item(
+        discord.ui.Button(
+            label="Roblox Profile",
+            url=f"https://roblox.com/users/{response.get('robloxID', 0)}/profile",
+        )
+    )
+
+    return (embed, view)
+
+
+bot.tree.add_command(api_menu)
