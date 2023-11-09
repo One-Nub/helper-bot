5487945216e0247e4992fe3f28896c482ff382aa
Nub <38899321+One-Nub@users.noreply.github.com>
2023-11-09 22:23:15 +0000

    Add context menu to translate a message

--- src/modules/commands/translate.py
+++ src/modules/commands/translate.py
@@ -4,13 +4,13 @@ import discord
 from discord.ext.commands import Context, check
 from googletrans import Translator
 
-from resources.checks import is_staff
+from resources.checks import is_staff_or_trial
 from resources.constants import BLURPLE, RED
 from resources.helper_bot import instance as bot
 
 
 @bot.command("translate", description="Translate text to different languages.", aliases=["tr"])
-@check(is_staff)
+@check(is_staff_or_trial)
 async def translate(ctx: Context, *, translate_string: str = "0"):
     """Translate provided text to another language."""
     try:
@@ -41,3 +41,37 @@ async def translate(ctx: Context, *, translate_string: str = "0"):
         embed_var.timestamp = datetime.now()
 
         await ctx.reply(embed=embed_var)
+
+
+@discord.app_commands.context_menu(name="Translate")
+async def translate_menu(interaction: discord.Interaction, message: discord.Message):
+    allowed_to_run = await is_staff_or_trial(interaction)
+    if not allowed_to_run:
+        await interaction.response.send_message(
+            content="You are not allowed to use this context menu!",
+            ephemeral=True,
+            allowed_mentions=discord.AllowedMentions(users=False),
+        )
+        return
+
+    translator = Translator()
+    translation = translator.translate(message.content, dest="en")
+
+    success_embed = discord.Embed()
+    success_embed.title = "<:BloxlinkHappy:823633735446167552> Translation Complete"
+    success_embed.description = (
+        f"Processed text from `{translation.src}` (detected) to "
+        f"`en`\n\n**Translated Text:**\n```{translation.text}```"
+    )
+    success_embed.color = BLURPLE
+    success_embed.timestamp = datetime.now()
+    success_embed.set_footer(text="Bloxlink Helper", icon_url=interaction.user.display_avatar)
+
+    await interaction.response.send_message(
+        embed=success_embed,
+        ephemeral=True,
+        allowed_mentions=discord.AllowedMentions(users=False),
+    )
+
+
+bot.tree.add_command(translate_menu)
