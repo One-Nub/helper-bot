7f9615cb06ba938bc93aff88034846a12262f488
Nub <38899321+One-Nub@users.noreply.github.com>
2025-05-15 16:54:13 +0000

    Add reply message when people ping the dev in replies

---
+++ src/modules/auto_response/dev_autoresponder.py
@@ -0,0 +1,62 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+from resources.constants import ADMIN_ROLES
+from resources.helper_bot import HelperBot
+
+
+@app_commands.guild_only()
+class DevAutoresponder(commands.GroupCog, name="dev_autoresponder"):
+    TIME_TO_DELETE = 10
+
+    def __init__(self, bot):
+        self.bot: HelperBot = bot
+        super().__init__()
+
+    @commands.Cog.listener("on_message")
+    async def message_handler(self, message: discord.Message):
+        if (
+            message.author.bot
+            or not message.guild
+            or not type(message.author) == discord.Member
+            or not message.reference
+        ):
+            return
+
+        referenced_message = message.reference.cached_message or await message.channel.fetch_message(
+            message.reference.message_id  # type: ignore
+        )
+        if not referenced_message:
+            # could not find the referenced message.
+            return
+
+        volunteers_role = ADMIN_ROLES["hq_volunteers"]
+        dev_role = ADMIN_ROLES["dev"]
+        author_roles = [x.id for x in message.author.roles]
+        if volunteers_role in author_roles or dev_role in author_roles:
+            # Let helpers & admins bypass it.
+            return
+
+        if type(referenced_message.author) == discord.User:
+            return
+
+        ref_author_roles = [x.id for x in referenced_message.author.roles]  # type: ignore
+        if dev_role not in ref_author_roles:
+            # allow other people to reply to each other
+            return
+
+        reply_msg = await message.reply(
+            content=(
+                "Please do not ping the developer when replying to him. "
+                "Send your message again, but do not ping him in your reply, thanks!\n"
+                "-# This is an automated message and will self destruct in 10 seconds."
+            )
+        )
+
+        await message.delete(delay=self.TIME_TO_DELETE)
+        await reply_msg.delete(delay=self.TIME_TO_DELETE)
+
+
+async def setup(bot: HelperBot):
+    await bot.add_cog(DevAutoresponder(bot))
