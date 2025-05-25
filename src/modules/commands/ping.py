138dfbf0acf40f6bb74bd50ab7005d2b578042e6
Nub <38899321+One-Nub@users.noreply.github.com>
2025-05-25 03:26:20 +0000

    Make ping a hybrid command

--- src/modules/commands/ping.py
+++ src/modules/commands/ping.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Context
 from resources.helper_bot import instance as bot
 
 
-@bot.command("ping", description="Check the bot's latency.")
+@bot.hybrid_command("ping", description="Check the bot's latency.")
 async def ping(ctx: Context):
     """Find out what the bot latency is!"""
     ## get the bot latency and round it, multiply by 1000
