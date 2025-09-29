6c82d62473dda43f94560de27f0e2a16899697a4
Nub <38899321+One-Nub@users.noreply.github.com>
2025-09-29 03:16:11 +0000

    Add right click ctx menu for getting discord user info

--- src/modules/commands/whois.py
+++ src/modules/commands/whois.py
@@ -1,4 +1,5 @@
 import traceback
+import typing
 from datetime import datetime
 
 import discord
@@ -24,19 +25,39 @@ async def whois(ctx: commands.Context, user: discord.User):
     except (discord.HTTPException, discord.NotFound):
         member = None
 
+    embed = whois_embed_builder(member if member is not None else user)
+    embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
+
+    await ctx.reply(embed=embed, mention_author=False)
+
+
+@discord.app_commands.context_menu(name="Get User Information")
+async def whois_menu(interaction: discord.Interaction, user: typing.Union[discord.Member, discord.User]):
+    embed = whois_embed_builder(user)
+    embed.set_footer(text="Bloxlink Helper", icon_url=interaction.user.display_avatar)
+
+    await interaction.response.send_message(
+        embed=embed, allowed_mentions=discord.AllowedMentions(replied_user=False), ephemeral=True
+    )
+
+
+def whois_embed_builder(user: typing.Union[discord.Member, discord.User]) -> discord.Embed:
     embed = discord.Embed(
         color=BLURPLE,
         timestamp=datetime.now(),
     )
 
-    name = f"@{user.name}" if user.discriminator == 0 else f"{user.name}#{user.discriminator}"
-    embed.set_author(name=f"{user.global_name} ({name})", icon_url=user.avatar)
+    name = f"@{user.name}" if user.discriminator == "0" else f"{user.name}#{user.discriminator}"
+    embed.set_author(
+        name=f"{user.global_name} ({name})" if user.global_name is not None else name, icon_url=user.avatar
+    )
     embed.set_thumbnail(url=user.avatar)
 
+    user_is_member = isinstance(user, discord.Member)
     embed.description = f"{user.mention}"
-    if member:
-        embed.description += f"\n**Nickname**: {member.nick}"
-    if not member:
+    if user_is_member:
+        embed.description += f"\n**Nickname**: {user.nick}"
+    if not user_is_member:
         embed.description += "\n*User is not a member of this server!*"
 
     embed.add_field(
@@ -44,17 +65,15 @@ async def whois(ctx: commands.Context, user: discord.User):
         value=f"<t:{int(user.created_at.timestamp())}:R> (<t:{int(user.created_at.timestamp())}>)",
         inline=True,
     )
-    if member:
+    if user_is_member:
         embed.add_field(
             name="Joined",
-            value=f"<t:{int(member.joined_at.timestamp())}:R> (<t:{int(member.joined_at.timestamp())}>)",  # type: ignore
+            value=f"<t:{int(user.joined_at.timestamp())}:R> (<t:{int(user.joined_at.timestamp())}>)",  # type: ignore
             inline=True,
         )
-        embed.add_field(name="Roles", value=", ".join([role.mention for role in member.roles]))
+        embed.add_field(name="Roles", value=", ".join([role.mention for role in user.roles]))
 
-    embed.set_footer(text="Bloxlink Helper", icon_url=ctx.author.display_avatar)
-
-    await ctx.reply(embed=embed, mention_author=False)
+    return embed
 
 
 @whois.error
@@ -78,4 +97,5 @@ async def whois_error(ctx: commands.Context, error: commands.CommandError):
     await ctx.reply(content=message[:2000], ephemeral=True, mention_author=False)
 
 
-async def setup(bot): ...
+async def setup(bot):
+    bot.tree.add_command(whois_menu)
